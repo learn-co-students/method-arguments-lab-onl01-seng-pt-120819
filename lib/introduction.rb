690b1555d43d09b0ec5e6ed0aaf860966244df13
Kelvin Vaughn Lee Jr <gtlp4l@gmail.com>
2019-12-10 22:01:07 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,5 +1,7 @@
 # lib/introduction.rb
 
 def introduction(name)
-  puts "Hi, my name is #{name}."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
+
+introduction( )
