621f0d656d0d597b52b4ba0e575349a603c79377
Alexis Priore <alexispriore@gmail.com>
2019-12-12 01:57:19 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,16 @@
+# lib/introduction.rb
+ 
+def introduction
+end
+
+def introduction(name)
+end
+
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+
+
+def greeting_programmer(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language."
+end
